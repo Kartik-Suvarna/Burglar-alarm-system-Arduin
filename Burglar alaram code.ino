80778935d79fa18adf3cb65bcbc261acc3ad8227
Kar7ik <73068433+Kartik-Suvarna@users.noreply.github.com>
2024-04-07 12:15:57 +0530

    Add files via upload

---
+++ Burglar alaram code.ino
@@ -0,0 +1,44 @@
+#include <SoftwareSerial.h>
+
+int pir_sensor = 11;
+int pin= 5;
+int buzz = 3;
+
+void setup() {
+  pinMode(pir_sensor, INPUT);    // initialize sensor as an input
+  pinMode(pin, OUTPUT);
+  pinMode(buzz, OUTPUT);
+  Serial.begin(600);          // initialize serial
+}
+
+void play(){
+  tone(buzz,300,345);
+  digitalWrite(pin, HIGH);
+  delay (700);
+  tone(buzz,300,345);
+  digitalWrite(pin, LOW);
+  delay (700);
+  tone(buzz,300,345);
+  digitalWrite(pin, HIGH);
+  delay (700);
+  tone(buzz,300,345);
+  digitalWrite(pin, LOW);
+  delay(700);`
+  tone(buzz,300,345);
+  digitalWrite(pin, HIGH);
+  delay(700);
+}
+
+void loop(){
+  int val = digitalRead(pir_sensor);  // read sensor value
+  if (val == HIGH) {                  // check if the sensor is HIGH
+    Serial.println("Motion detected!");
+    play();
+    play();
+  }
+  else{
+    Serial.println("Motion not detected!");
+    digitalWrite(pin, LOW);
+    digitalWrite(buzz, LOW);
+  }
+}
